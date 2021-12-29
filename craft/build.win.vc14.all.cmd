2fc1d5db19ac5f3b6f90b571a4f25dad3d637a93
View <ms_view@outlook.com>
2021-12-29 21:21:53 +0800

    使用 VS 2019+ 构建

--- craft/build.win.vc14.all.cmd
+++ craft/build.win.vc14.all.cmd
@@ -11,3 +11,8 @@ call %~dp0build.win.vc14.x86.cmd
 
 echo ^=^= building x86.debug ^=^=
 call %~dp0build.win.vc14.x86.debug.cmd
+
+move "../../../build/lib/x64/SeetaQualityAssessor300.dll" "../../../build//bin/x64/"
+move "../../../build/lib/x64/SeetaQualityAssessor300d.dll" "../../../build//bin/x64/"
+move "../../../build/lib/x86/SeetaQualityAssessor300.dll" "../../../build//bin/x86/"
+move "../../../build/lib/x86/SeetaQualityAssessor300d.dll" "../../../build//bin/x86/"
